31b0cb47c703c9003ae6a7465b3af60b09251233
Marco Herremans <contact@marcoherremans.nl>
2017-03-26 19:49:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ mysql_connect("localhost","DBLOGINNAME","DBLOGINPASS")or die("cannot connect");
 mysql_select_db("DBNAME")or die("cannot select DB");
 mysql_query("INSERT INTO `TABLENAME` (`ROW1`, `ROW2`) VALUES ('ROW1VALUE', AES_ENCRYPT('ROW2VALUE', 'ENCRYPTIONCODE'))");
 
-$sql = mysql_query("SELECT AES_DECRYPT(ROW2,  'ENCRYPTIONCODE')  FROM `TABLENAME` where ROW1 = 'VARIABLE');
+$sql = mysql_query("SELECT AES_DECRYPT(ROW2,  'ENCRYPTIONCODE')  FROM `TABLENAME` where ROW1 = 'VALUE');
 
 while ($row = mysql_fetch_array($sql))
 {
